751d47ffb58184c5ebb73c85373fc8c50b41ad96
michaelyin <gt8442b@yahoo.com>
2016-03-25 07:39:18 -0400

    org.picocontainer.ComponentAdapter is deprecated. To make it work in plf 4.3, need to use a different ComponentAdapter.

--- src/main/java/org/exoplatform/commons/juzu/KernelProviderFactory.java
+++ src/main/java/org/exoplatform/commons/juzu/KernelProviderFactory.java
@@ -1,24 +1,42 @@
 package org.exoplatform.commons.juzu;
 
+import java.net.URL;
+
 import javax.inject.Provider;
+
 import juzu.inject.ProviderFactory;
+
 import org.exoplatform.container.PortalContainer;
-import org.picocontainer.ComponentAdapter;
+import org.exoplatform.services.log.ExoLogger;
+import org.exoplatform.services.log.Log;
+//import org.picocontainer.ComponentAdapter;
+import org.exoplatform.container.spi.ComponentAdapter;
 
 public class KernelProviderFactory implements ProviderFactory {
+	
+	private static final Log LOG = ExoLogger.getLogger(KernelProviderFactory.class.getName()); 
 
     @Override
     public <T> Provider<? extends T> getProvider(final Class<T> implementationType) throws Exception {
+    	Class klass = PortalContainer.class;
+        URL location = klass.getResource('/'+klass.getName().replace('.', '/')+".class");
+        LOG.info("get container from " + location);
+        
+    	LOG.info("get provider for " + implementationType.getName());
         final PortalContainer container = PortalContainer.getInstance();
+        
         if (container == null) {
-            throw new IllegalStateException("Not running in the context of a portal container");
+          //  throw new IllegalStateException("Not running in the context of a portal container");
+        	LOG.error("Not running in the context of a portal container");
+        	return null;
         }
-        final ComponentAdapter adapter = container.getComponentAdapterOfType(implementationType);
+        
+        final ComponentAdapter<T> adapter = container.getComponentAdapterOfType(implementationType);
         if (adapter != null) {
             return new Provider<T>() {
                 @Override
                 public T get() {
-                    Object service = adapter.getComponentInstance(container);
+                    Object service = adapter.getComponentInstance(); //getComponentInstance(container);
                     if (service == null) {
                         throw new RuntimeException("Could not obtain service " + implementationType + " from container " + container);
                     }
