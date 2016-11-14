9ca87856ab462fa0e262211b3c448ea486481077
michaelyin <gt8442b@yahoo.com>
2016-11-13 19:54:05 -0500

    happy path to save standard is done. Needs to add exception handling and logic to handle the updae/delete of the standard.

--- src/main/java/org/juzu/tutorial/NationalStandardController.java
+++ src/main/java/org/juzu/tutorial/NationalStandardController.java
@@ -11,6 +11,7 @@ import org.exoplatform.services.log.ExoLogger;
 import org.exoplatform.services.log.Log;
 import org.exoplatform.services.security.ConversationState;
 import org.exoplatform.services.security.Identity;
+import org.json.JSONObject;
 
 import juzu.Path;
 import juzu.Resource;
@@ -22,6 +23,10 @@ import juzu.request.SecurityContext;
 import juzu.template.Template;
 import net.wyun.qys.domain.Policy;
 import net.wyun.qys.domain.UserSetting;
+import net.wyun.qys.domain.standard.StanJcrFile;
+import net.wyun.qys.domain.standard.StanTag;
+import net.wyun.qys.domain.standard.Standard;
+import net.wyun.qys.domain.standard.StandardType;
 import net.wyun.qys.service.PolicyService;
 import net.wyun.qys.service.StandardService;
 import net.wyun.qys.service.UserService;
@@ -36,6 +41,9 @@ public class NationalStandardController {
 	  
 	  @Inject
 	  StandardService standardSvc;
+	  
+	  @Inject
+	  DocumentsDataHelper documentsData;
 	  
 	  @Inject
 	  @Path("standard.gtmpl")
@@ -86,17 +94,77 @@ public class NationalStandardController {
 	
 	  @Resource
 	  @Ajax
-	  public Response.Content upload(String author, String department, String selectTag, String standardType, List<FileItem> files){
+	  public Response.Content upload(String standardName, String standardNum, Integer standardTypeString, 
+			                         String perm, String encrpLevel, String text, 
+			                         String author, String department, String selectTag, Integer standardType, 
+			                         List<FileItem> files){
 		  
 		  LOG.info("author: " + author + ", department: " + department + ", selectTag: " + selectTag + ", type: " + standardType);
+		  
+		  Standard s = new Standard();
+		  s.setName(standardName);
+		  s.setCreateDate(new Date());
+		  s.setNum(standardNum);
+		  s.setType(StandardType.typeForValue(standardType));
+		  s.setUuid("");   //text being saved to a file
+		  
+		  StanTag sTag1 = new StanTag();
+		  sTag1.setTag(selectTag);
+		  s.addStanTag(sTag1);
+		
+		  Standard newS = standardSvc.save(s);
+		  String folderName = newS.getId();
+		  
+		  //create jcr folder here
+		  boolean isCreated = documentsData.createNodeIfNotExist("Documents/fs", folderName);
+		  LOG.info(folderName + " folder is created!");
+		  
 		  if(null != files){
 			  for(FileItem fi:files){
 	        	  LOG.info("file name: " + fi.getName());
+	        	  
+	        	  String uuid = documentsData.storeFile("fs/" + folderName , fi, documentsData.getSpaceName(), false, null);
+	        	  StanJcrFile jFile = new StanJcrFile();
+	    		  jFile.setFileName(fi.getName());
+	    		  jFile.setUploadDate(new Date());
+	    		  jFile.setUrl("temp/url");
+	    		  jFile.setUuid(uuid);
+	    		  newS.addStanJcrFile(jFile);
+	    		  standardSvc.update(newS);
 	          }
 		  }
           
+		  return Response.ok(new JSONObject(newS).toString()).withMimeType("text/json");
+		  /*
 		  return Response.ok("{\"status\":\"File has been uploaded successfully!\"}")
                   .withMimeType("application/json; charset=UTF-8").withHeader("Cache-Control", "no-cache");
+                  */
+	  }
+	  
+	  private void saveStandard(){
+		  Standard s = new Standard();
+		  s.setName("国家标准2016");
+		  s.setCreateDate(new Date());
+		  s.setNum("001-2960-2016");
+		  s.setType(StandardType.IMB);
+		  s.setUuid("4eb65550-a36a-11e6-80f5-76304dec7eb7");
+		  
+		  StanJcrFile jFile = new StanJcrFile();
+		  jFile.setFileName("testfile.txt");
+		  jFile.setUploadDate(new Date());
+		  jFile.setUrl("temp/url");
+		  jFile.setUuid("4eb65550-a36a-11e6-0000-76304dec7eb7");
+		  
+		  s.addStanJcrFile(jFile);
+		  
+		  StanTag sTag1 = new StanTag();
+		  sTag1.setTag("first tag: car market 上海");
+		  s.addStanTag(sTag1);
+		  
+		  StanTag sTag = new StanTag();
+		  sTag.setTag("second tag: car market 北京");
+		  s.addStanTag(sTag);
+		  
 	  }
 	  
 	  
