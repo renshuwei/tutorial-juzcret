833ce23a1c2fa3058103e4e493b0a24989fd84da
michaelyin <gt8442b@yahoo.com>
2016-11-26 16:39:23 -0500

    search now is working.

--- src/main/java/org/juzu/tutorial/assets/js/nationalsd/standard.js
+++ src/main/java/org/juzu/tutorial/assets/js/nationalsd/standard.js
@@ -1,5 +1,9 @@
 var history_array = [];
 var suffix = 0;
+var page_size = 10;
+var page_num = 1;
+localStorage.json_str ="";
+
 var _gauges = _gauges || [];
 (function() {
 	var t = document.createElement('script');
@@ -13,13 +17,119 @@ var _gauges = _gauges || [];
 })();
 
 
-$("#tcdPageCode").createPage({
-		pageCount:20,
-		current:1,
-		backFn:function(p){
+
+(function($){
+
+	$(document).ready(function(){
+		
+		$("#tcdPageCode").createPage({
+			pageCount:20,
+			current:1,
+			backFn:function(p){
+				
+		    }
+	    });
+		
+		$("#search").click(function(){
+
+			//validate form data before sending it to server
+			if(validateForm()){
+				  var search_text = document.getElementById("search_text").value;
+				   //alert(search_text);
+				   /*if(search_text.length>20)
+				   {
+					   search_text = search_text.substr(0, 20);
+				   }*/
+				   get_history(history_array);
+				   
+				  
+				   if(suffix == 10)
+				   {
+					   suffix = 0;
+				   }
+				   if(search_text=="")
+				   {
+					    history_array[suffix] = "搜索所有数据";
+				   }
+				   else
+				   {
+					   history_array[suffix] = "搜索条件："+search_text;
+				   } 
+				   suffix = suffix+1;
+				   
+				   $("#form1").submit();
+			};
 			
-	}
-});
+		});
+		
+    
+		$("#form1").submit(function(e) {
+			 
+		    var formObj = $(this);
+		    var formURL = formObj.attr("action");
+		    var formData = new FormData(this);
+		    $.ajax({
+		        url: formURL,
+		        type: 'POST',
+		        data:  formData,
+		        dataType: 'text',
+		        mimeType:"multipart/form-data",
+		        contentType: false,
+		        cache: false,
+		        processData:false,
+		    success: function(data, textStatus, jqXHR)
+		    {
+		       console.log("post success: " + data);
+		       //handle data here:
+		       processJson(data);
+		       
+		    },
+		     error: function(jqXHR, textStatus, errorThrown) 
+		     {
+		    	 alert('递交数据失败： ' + errorThrown);
+		     }          
+		    });
+		   // console.log("type of e: " + type(e));
+		    e.preventDefault(); //Prevent Default action. 
+		   // e.unbind();
+         }); 
+		
+		function validateForm()
+		{
+			/*
+			if($("#form1").find("input[name='standardName']").val()=="".trim())
+			{
+				alert("请输入标准名称！");
+				return false;
+			}
+			if($("#form1").find("input[name='standardNum']").val()=="".trim())
+			{
+				alert("请输入标准编号！");
+				return false;
+			}
+			if($("#form1").find("input[name='standardTypeTxt']").val()=="".trim())
+			{
+				alert("请输入标准类型！");
+				return false;
+			}
+			if($("#form1").find("input[name='tag']").val()=="".trim())
+			{
+				alert("请输入标签!");
+				return false;
+			}
+			if($("#form1").find("textarea[name='text']").val()=="".trim())
+			{
+				alert("请输入标准原文！");
+				return false;
+			}
+			*/
+			return true;	
+		}
+		
+		
+	})//document ready
+})($);
+
 
 
 function update_data()
@@ -103,95 +213,40 @@ var path;
 				return;
 			}
 			
-			$("#"+add_object.form).ajaxSubmit({
-				url: path,
-            	type: 'POST',
-				dataType: 'json',
-				success: function (json) {
-					//alert(json.resultList.length);
-					//alert(JSON.stringify(json));
-					var result_table = "";
-					for(var i=0; i < json.resultList.length;i++)
-					{
-						
-						var name_value = json.resultList[i].name;
-						//alert(name_value);
-						//var json_value = JSON.stringify(json.resultList[i]);
-						var num_value = json.resultList[i].num;
-						//alert(num_value);
-						var type_value = json.resultList[i].type;
-						//alert(type_value);
-						var txt_value = json.resultList[i].txt;
-						//alert(txt_value);
-						var files_str = json.resultList[i].stanJcrFiles[0];
-						//alert(files_str);
-						var file_str_arr = new Array();
-						file_str_arr = files_str.split("},"); //字符分割
-						file_value =""
-						for (j=0;j<file_str_arr.length ;j++ )
-						{
-							if(j!=(file_str_arr.length-1))
-							{
-								
-								var file_str = file_str_arr[j]+'}';
-								
-							}
-							else
-							{
-								var file_str = file_str_arr[j]
-							}
-							var file_obj=eval('('+file_str+')');
-							//alert(file_obj.fileName)
-							var file_value = file_value+file_obj.fileName+';';
-						}
-						//alert(file_value);
-						var tag_str = json.resultList[i].stanTags[0];
-						var tagobj=eval('('+tag_str+')');
-						var tag_value = tagobj.tag;
-						//alert(tag_value);
-						//alert(json_value);
-						var json_value ="name="+name_value+"&num="+num_value+"&type="+type_value+"&tag="+tag_value+"&txt="+txt_value+"&files="+file_value+"&new=0&update=1";
-						var result_table = result_table + "<tr>"+
-                    							"<td>"+
-                        							"<div class=\"tooltips\">"+
-                        								"<a href=\"standards_upload.html?"+json_value+"\">"+name_value+"</a>"+
-                        								"<div class=\"tooltips1\">";
-														if(i<=5)
-														{
-															result_table = result_table +"<span class=\"top2\">显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示。</span>";
-														}
-														else
-														{
-															result_table = result_table +"<span class=\"top1\">显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示。</span>";
-														}                        									
-                            							result_table = result_table +"</div>"+
-                         							"</div>"+
-                        						"</td>"+
-                        						"<td>"+num_value+"</td>"+
-                        						"<td>"+tag_value+"</td>"+
-                   							"</tr>";/**/
-						
-						
-						
-					}
-					//alert(result_table);
-					
-					var result_div = document.getElementById("result_div");
-  					result_div.innerHTML = result_table;
-					var total = document.getElementById("total");
-  					total.innerHTML = "共有"+json.resultList.length+"条政策符合要求";
-					
-					
-          		},//submit success
+			$("#form1").submit();
 			
-				error: function (data)
-				{
-					alert("json接口出错！")
-					//alert("bbbbb");
-					//displayProp(data);
-				}
-			}); //ajaxSubmit
+			$("#form1").submit(function(e) {
+				 
+			    var formObj = $(this);
+			    var formURL = formObj.attr("action");
+			    var formData = new FormData(this);
+			    $.ajax({
+			        url: formURL,
+			        type: 'POST',
+			        data:  formData,
+			        dataType: 'text',
+			        mimeType:"multipart/form-data",
+			        contentType: false,
+			        cache: false,
+			        processData:false,
+			    success: function(data, textStatus, jqXHR)
+			    {
+			       console.log("post success: " + data);
+			       //handle data here:
+			       processJson(data);
+			       
+			    },
+			     error: function(jqXHR, textStatus, errorThrown) 
+			     {
+			    	 alert('递交数据失败： ' + errorThrown);
+			     }          
+			    });
+			   // console.log("type of e: " + type(e));
+			    e.preventDefault(); //Prevent Default action. 
+			   // e.unbind();
+	         }); 
 			
+			 
 		}
 		return add_object;
 	}
@@ -208,6 +263,139 @@ var path;
 	//document.getElementById("createDate").value = getNowFormatDate();
 	//alert(get_url_base());
 	
+	
+	function processJson(data){
+		//alert(json.resultList.length);
+		//alert(JSON.stringify(json));
+		var json = JSON.parse(data);
+		localStorage.json_str = (data);
+		//alert(localStorage.json_str);
+		var total = document.getElementById("total");
+		
+		total.innerHTML = "共有"+json.resultList.length+"条政策符合要求";
+		//alert((json.resultList.length)%page_siz);
+		if(json.resultList.length%page_size==0)
+		{
+			var page_count = json.resultList.length/page_size;
+		}
+		else
+		{
+			var page_count = parseInt(json.resultList.length/page_size)+1;
+		}
+		//alert(page_count);
+		get_page(page_count,json);
+		get_page_result(json,page_num);
+	}
+	
+	function get_page(page_count)
+	{
+		$("#tcdPageCode").createPage({
+				pageCount:page_count,
+				current:1,
+				backFn:function(p){
+					
+			}
+		});
+	}
+	
+	function get_page_num(page_num)
+	{
+		//alert("页面"+page_num);
+		json = JSON.parse(localStorage.json_str);
+		get_page_result(json,page_num);
+	}
+	
+	//display page results
+	function get_page_result(json,page_num)
+	{
+		var result_table = "";
+		var page_start = (page_num-1)*page_size;
+		if(json.resultList.length < (page_start+page_size))
+		{
+			var result_count = json.resultList.length;
+		}
+		else
+		{
+			var result_count = page_start+page_size;
+		}
+		for(var i=page_start; i < result_count;i++)
+		{
+			
+			var name_value = json.resultList[i].name;
+			//alert(name_value);
+			//var json_value = JSON.stringify(json.resultList[i]);
+			var num_value = json.resultList[i].num;
+			//alert(num_value);
+			var type_value = json.resultList[i].type;
+			//alert(type_value);
+			var txt_value = json.resultList[i].txt;
+			//alert(txt_value);
+			var id_value = json.resultList[i].id;
+			var dept_value = json.resultList[i].dept;
+			var createDate_value = json.resultList[i].createDate;
+			var files_str = json.resultList[i].stanJcrFiles[0];
+			//alert(files_str);
+			var file_str_arr = new Array();
+			file_str_arr = files_str.split("},"); //字符分割
+			var file_value = "";
+			var uuid_value = "";
+			for (j=0;j<file_str_arr.length ;j++ )
+			{
+				if(j!=(file_str_arr.length-1))
+				{
+					
+					var file_str = file_str_arr[j]+'}';
+					
+				}
+				else
+				{
+					var file_str = file_str_arr[j]
+				}
+				var file_obj=eval('('+file_str+')');
+				//alert(file_obj.fileName)
+				var file_value = file_value+file_obj.fileName+';';
+				var uuid_value = uuid_value+file_obj.uuid+';';
+			}
+			//alert(uuid_value);
+			var tag_str = json.resultList[i].stanTags[0];
+			var tagobj=eval('('+tag_str+')');
+			var tag_value = tagobj.tag;
+			var tag_value = tag_value.replace(/\s+/g,"、");
+			//alert(tag_value);
+			//alert(json_value);
+			var json_value ="id="+id_value+"&name="+name_value+"&num="+num_value+"&dept="+dept_value+"&createDate="+createDate_value+"&type="+type_value+"&tag="+tag_value+"&txt="+txt_value+"&files="+file_value+"&urls="+uuid_value+"&new=0&update=1";
+			var result_table = result_table + "<tr>"+
+									"<td>"+
+										"<div class=\"tooltips\">"+
+											"<a href=\"standards_content.html?"+json_value+"\">"+name_value+"</a>"+
+											"<div class=\"tooltips1\">";
+											
+											if(i<=(page_start+parseInt((result_count-page_start+1)/2)))
+											{
+												
+												result_table = result_table +"<span class=\"top2\">显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示。</span>";
+												
+											}
+											else
+											{
+												result_table = result_table +"<span class=\"top1\">显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示注释显示。</span>";
+											}                        									
+											result_table = result_table +"</div>"+
+										"</div>"+
+									"</td>"+
+									"<td>"+num_value+"</td>"+
+									"<td>"+tag_value+"</td>"+
+								"</tr>";
+			
+			
+			
+		}
+		//alert(result_table);
+		
+		var result_div = document.getElementById("result_div");
+		result_div.innerHTML = result_table;
+		
+	}
 	
 
 function get_search()
